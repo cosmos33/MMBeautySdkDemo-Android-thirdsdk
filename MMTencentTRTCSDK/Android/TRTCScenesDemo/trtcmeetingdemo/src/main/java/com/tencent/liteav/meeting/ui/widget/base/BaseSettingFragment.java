0a752f6a20be8e22f6000cce7b82f8ede15ddcde
fanqiang <fan.qiang_2499@immomo.com>
2021-06-04 11:07:39 +0800

    支持腾讯TRTC

---
+++ MMTencentTRTCSDK/Android/TRTCScenesDemo/trtcmeetingdemo/src/main/java/com/tencent/liteav/meeting/ui/widget/base/BaseSettingFragment.java
@@ -0,0 +1,46 @@
+package com.tencent.liteav.meeting.ui.widget.base;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.tencent.liteav.meeting.model.TRTCMeeting;
+
+/**
+ * 设置fragment的基类
+ *
+ * @author guanyifeng
+ */
+public abstract class BaseSettingFragment extends Fragment {
+    protected Handler     mHandler = new Handler();
+    protected TRTCMeeting mTRTCMeeting;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(getLayoutId(), container, false);
+    }
+
+    public TRTCMeeting getTRTCMeeting() {
+        return mTRTCMeeting;
+    }
+
+    public void setTRTCMeeting(TRTCMeeting TRTCMeeting) {
+        mTRTCMeeting = TRTCMeeting;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
+
+    protected abstract void initView(View view);
+
+    protected abstract int getLayoutId();
+}
---
+++ mmbeautybase/src/main/java/com/cosmos/thirdlive/TencentTRTCBeautyManager.java
@@ -0,0 +1,55 @@
+package com.cosmos.thirdlive;
+
+import android.content.Context;
+
+import com.core.glcore.util.ImageFrame;
+import com.cosmos.appbase.BeautyManager;
+import com.cosmos.beauty.model.MMRenderFrameParams;
+import com.cosmos.beauty.model.datamode.CommonDataMode;
+import com.cosmos.beautyutils.Empty2Filter;
+import com.cosmos.beautyutils.FaceInfoCreatorPBOFilter;
+import com.cosmos.beautyutils.RotateFilter;
+
+/**
+ * 腾讯TRTC接入美颜sdk管理类
+ */
+public class TencentTRTCBeautyManager extends BeautyManager {
+    private RotateFilter rotateFilter;
+
+    public TencentTRTCBeautyManager(Context context) {
+        super(context, cosmosAppid);
+    }
+
+    @Override
+    public int renderWithTexture(int texture, int texWidth, int texHeight, boolean mFrontCamera) {
+        if (resourceReady) {
+            if (rotateFilter == null) {
+                rotateFilter = new RotateFilter(RotateFilter.ROTATE_VERTICAL);
+                faceInfoCreatorPBOFilter = new FaceInfoCreatorPBOFilter(texWidth, texHeight);
+                emptyFilter = new Empty2Filter();
+                emptyFilter.setWidth(texWidth);
+                emptyFilter.setHeight(texHeight);
+            }
+            int rotateTexture = rotateFilter.rotateTexture(texture, texWidth, texHeight);
+            faceInfoCreatorPBOFilter.newTextureReady(rotateTexture, emptyFilter, true);
+            if (faceInfoCreatorPBOFilter.byteBuffer != null) {
+                byte[] frameData = new byte[faceInfoCreatorPBOFilter.byteBuffer.remaining()];
+                faceInfoCreatorPBOFilter.byteBuffer.get(frameData);
+                //美颜sdk处理
+                CommonDataMode dataMode = new CommonDataMode();
+                dataMode.setNeedFlip(mFrontCamera);
+                int beautyTexture = renderModuleManager.renderFrame(rotateTexture, new MMRenderFrameParams(
+                        dataMode,
+                        frameData,
+                        texWidth,
+                        texHeight,
+                        texWidth,
+                        texHeight,
+                        ImageFrame.MMFormat.FMT_RGBA
+                ));
+                return rotateFilter.rotateTexture(beautyTexture, texWidth, texHeight);
+            }
+        }
+        return texture;
+    }
+}
