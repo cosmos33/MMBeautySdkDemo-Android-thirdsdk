52678be34dcd544396671f05de9faa485e963b71
fanqiang <fan.qiang_2499@immomo.com>
2021-12-16 11:22:35 +0800

    修改：兼容腾讯回调在没有looper的线程调用的情况

--- MMTencent/app/src/main/java/com/tencent/liteav/demo/livepusher/camerapush/CameraPushTxVideoCaptureImpl.java
+++ MMTencent/app/src/main/java/com/tencent/liteav/demo/livepusher/camerapush/CameraPushTxVideoCaptureImpl.java
@@ -1,43 +1,13 @@
 package com.tencent.liteav.demo.livepusher.camerapush;
 
 import android.content.Context;
+import android.os.Looper;
 
-import androidx.multidex.BuildConfig;
-
-import com.core.glcore.cv.MMCVInfo;
-import com.core.glcore.util.ImageFrame;
-import com.cosmos.appbase.listener.OnFilterResourcePrepareListener;
-import com.cosmos.appbase.listener.OnStickerResourcePrepareListener;
-import com.cosmos.appbase.utils.FilterUtils;
-import com.cosmos.beauty.CosmosBeautySDK;
-import com.cosmos.beauty.inter.OnAuthenticationStateListener;
-import com.cosmos.beauty.inter.OnBeautyResourcePreparedListener;
-import com.cosmos.beauty.model.AuthResult;
-import com.cosmos.beauty.model.BeautySDKInitConfig;
-import com.cosmos.beauty.model.MMRenderFrameParams;
-import com.cosmos.beauty.model.datamode.CommonDataMode;
-import com.cosmos.beauty.module.IMMRenderModuleManager;
-import com.cosmos.beauty.module.beauty.IBeautyModule;
-import com.cosmos.beauty.module.beauty.SimpleBeautyType;
-import com.cosmos.beauty.module.lookup.ILookupModule;
-import com.cosmos.beauty.module.sticker.DetectRect;
-import com.cosmos.beauty.module.sticker.IStickerModule;
-import com.cosmos.beauty.module.sticker.MaskLoadCallback;
-import com.cosmos.beautyutils.Empty2Filter;
-import com.cosmos.beautyutils.FaceInfoCreatorPBOFilter;
-import com.cosmos.beautyutils.RotateFilter;
 import com.cosmos.thirdlive.TencentBeautyManager;
 import com.immomo.resdownloader.utils.MainThreadExecutor;
-import com.mm.mmutil.toast.Toaster;
-import com.momo.mcamera.mask.MaskModel;
 import com.tencent.liteav.demo.livepusher.camerapush.model.CameraPushImpl;
 import com.tencent.rtmp.TXLivePusher;
 import com.tencent.rtmp.ui.TXCloudVideoView;
-
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
 
 public class CameraPushTxVideoCaptureImpl extends CameraPushImpl implements TXLivePusher.VideoCustomProcessListener {
     private TXCloudVideoView mPusherView;
@@ -79,10 +49,17 @@ public class CameraPushTxVideoCaptureImpl extends CameraPushImpl implements TXLi
 
     @Override
     public int onTextureCustomProcess(int texture, int texWidth, int texHeight) {
-        if (tencentBeautyManager == null) {
-            tencentBeautyManager = new TencentBeautyManager(mContext);
+        if (Looper.myLooper() == null) {
+            MainThreadExecutor.post(() -> {
+                if (tencentBeautyManager == null) {
+                    tencentBeautyManager = new TencentBeautyManager(mContext);
+                }
+            });
         }
-        return tencentBeautyManager.renderWithTexture(texture, texWidth, texHeight, true);
+        if (tencentBeautyManager != null) {
+            return tencentBeautyManager.renderWithTexture(texture, texWidth, texHeight, true);
+        }
+        return texture;
     }
 
     @Override
